54f6fc61e905dec246426297fc72a2156c4e1f1d
Zeeshan1234 <mayub9799@gmail.com>
2024-06-01 14:28:30 +0500

    blog posts view

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,39 @@
-import React from "react"
+import React from 'react'
+import { useState, useEffect } from 'react'
+import PostCard from './projects/PostCard'
+import particlesOptions from "../particles.json";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 
 export default function Posts() {
+    const post = {
+        "image": "images/blog/pwncollege.jpeg",
+        "title": "How I pwned pwn.college",
+        "bodyText": "This post is about the time I found a Cross Site Scripting vulnerability in pwn.college, going into the motivations and how I discovered it along with the possible impact and remediation.",
+        "tags": [
+            "XSS",
+            "CTF",
+            "pwn.college",
+        ]
+    }
+
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        if (init) {
+            return;
+        }
+        initParticlesEngine(async (engine) => {
+            await loadFull(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, [init]);
+
     return (
-        <div className="Posts">
-            <h1>Posts</h1>
+        <div>
+            {init && <Particles options={particlesOptions} />}
+            <h1 className='my-4'>Posts</h1>
+            <PostCard project={post} />
         </div>
     )
 }
---
+++ src/components/projects/PostCard.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import { Badge, Card } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { ThemeContext } from "styled-components";
+import ReactMarkdown from "react-markdown";
+
+const styles = {
+  cardTitleStyle: {
+    fontSize: 24,
+    fontWeight: 700,
+    marginBottom: 10,
+  },
+  buttonStyle: {
+    margin: 5,
+  },
+};
+
+const PostCard = (props) => {
+  const theme = useContext(ThemeContext);
+
+  const { project } = props;
+
+  return (
+    <div>
+      <Card
+        style={{
+          borderRadius: 10,
+          backgroundColor: theme.cardBackground,
+          borderColor: theme.cardBorderColor,
+          width: 500,
+          height: 325,
+          marginLeft: 100,
+          marginRight: 100,
+          marginBottom: 50,
+        }}
+        text={theme.bsSecondaryVariant}
+      >
+        <Card.Img variant="top" src={project?.image} style={{ height: 175 }} />
+        <Card.Body style={{ minHeight: "150px" }}>
+          <Card.Title style={styles.cardTitleStyle}>{project.title}</Card.Title>
+          <Card.Text style={{ textAlign: 'left' }} as="div">
+            <ReactMarkdown>{project.bodyText}</ReactMarkdown>
+          </Card.Text>
+        </Card.Body>
+
+        {project.tags && (
+          <Card.Footer style={{ backgroundColor: theme.cardFooterBackground }}>
+            {project.tags.map((tag) => (
+              <Badge
+                key={tag}
+                pill
+                bg={theme.bsSecondaryVariant}
+                text={theme.bsPrimaryVariant}
+                style={styles.badgeStyle}
+              >
+                {tag}
+              </Badge>
+            ))}
+          </Card.Footer>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  project: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    bodyText: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        href: PropTypes.string.isRequired,
+      })
+    ),
+    tags: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
+
+export default PostCard;
