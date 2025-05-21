0e673efc4d6f6c913c4f6d14f3821226e0296a21
Zeeshan Ayub <mayub9799@gmail.com>
2025-05-21 17:39:24 +0500

    remove typewriter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Typewriter from 'typewriter-effect';
 import { Fade } from 'react-awesome-reveal';
 import Social from './Social';
-import FallbackSpinner from './FallbackSpinner';
-import home from '../constants/home.json';
 import particlesOptions from "../particles.json";
-import Particles, {initParticlesEngine} from "@tsparticles/react";
-import {loadFull} from "tsparticles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
 
 const styles = {
   nameStyle: {
@@ -26,11 +23,9 @@ const styles = {
 
 
 function Home() {
-  const [data, setData] = useState(null);
   const [init, setInit] = useState(false);
 
   useEffect(() => {
-    setData(home)
     if (init) {
       return;
     }
@@ -41,25 +36,15 @@ function Home() {
     });
   }, [init]);
 
-  return data ? (
+  return (
     <div style={styles.mainContainer}>
-      {init && <Particles options={particlesOptions}/>}
+      {init && <Particles options={particlesOptions} />}
       <Fade>
-        <h1 style={styles.nameStyle}>{data?.name}</h1>
-        <div style={{ flexDirection: 'row' }}>
-          <h2 style={styles.inlineChild}>I&apos;m&nbsp;</h2>
-          <Typewriter
-            options={{
-              loop: true,
-              autoStart: true,
-              strings: data?.roles,
-            }}
-          />
-        </div>
+        <h1 style={styles.nameStyle}>Zeeshan1234</h1>
         <Social />
       </Fade>
     </div>
-  ) : <FallbackSpinner />;
+  );
 }
 
 export default Home;
