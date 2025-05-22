02839cae00c34c33dc1337c79c55686de5120221
Zeeshan Ayub <mayub9799@gmail.com>
2025-05-22 17:45:32 +0500

    rework blog page

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,7 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import post1Data from '../constants/pwncollege.md';
+import post2Data from '../constants/obscure.md';
 import { useParams } from 'react-router-dom';
 
 export default function Post() {
@@ -16,20 +17,23 @@ export default function Post() {
                 .then(response => response.text())
                 .then(text => setPost(text));
         } else if (id === "2") {
-        fetch("https://raw.githubusercontent.com/Zeeshan12340/writeups/main/obscure.md")
-            .then(response => response.text())
-            .then(text => setPost(text));
+            fetch(post2Data)
+                .then(response => response.text())
+                .then(text => setPost(text));
         }
     }, [id]);
 
     return (
-        <div className="my-5">
-            <div style={{ maxWidth: '1000px', margin: '0 auto', textAlign: 'justify', fontFamily: 'Inter, Sans-Serif' }}>
+        <div className="mt-5 pt-5" style={{ background: "linear-gradient(135deg, rgb(30, 30, 45), rgb(32, 32, 60))"}}>
+            <div style={{ maxWidth: '1000px', margin: '0 auto', paddingLeft: 10, textAlign: 'justify', fontFamily: 'Inter, Sans-Serif' }}>
                 <ReactMarkdown
                     components={{
                         img: ({ ...props }) => <div><img {...props} /></div>,
                         code: ({ inline, className, children, ...props }) => {
                             const match = /language-(\w+)/.exec(className || '');
+                            if (!inline && !match) {
+                                return  <SyntaxHighlighter style={dracula} language="bash" PreTag="div" {...props}>{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+                            }
                             return !inline && match ? (
                                 <SyntaxHighlighter style={dracula} language={match[1]} PreTag="div" {...props}>{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
                             ) : (
@@ -37,7 +41,8 @@ export default function Post() {
                                     {children}
                                 </code>
                             );
-                        }
+                        },
+                        h1: ({ ...props }) => <h1 className="text-3xl font-bold" {...props} />,
                     }}
                 >
                     {post}
